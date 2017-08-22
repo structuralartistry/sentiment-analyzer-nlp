dbee268ee416b053529f34cd892bb4d73a6348ab
Alexi Karuna <alexi@alexikaruna.com>
2017-08-22 12:24:13 -0500

    misc prior uncommitted

--- src/main/scala/ParagraphAnalyzer.scala
+++ src/main/scala/ParagraphAnalyzer.scala
@@ -1,20 +1,37 @@
 package com.shekhargulati.sentiment_analyzer
 
 import java.util.Properties
-
 import com.shekhargulati.sentiment_analyzer.Sentiment.Sentiment
 import edu.stanford.nlp.ling.CoreAnnotations
 import edu.stanford.nlp.neural.rnn.RNNCoreAnnotations
 import edu.stanford.nlp.pipeline.{Annotation, StanfordCoreNLP}
 import edu.stanford.nlp.sentiment.SentimentCoreAnnotations
-
 import scala.collection.convert.wrapAll._
+import play.api.libs.json._
+import scala.collection.mutable.ListBuffer
 
 object SentimentAnalyzer {
 
   val props = new Properties()
   props.setProperty("annotators", "tokenize, ssplit, parse, sentiment")
   val pipeline: StanfordCoreNLP = new StanfordCoreNLP(props)
+
+  def processJson(input: play.api.libs.json.JsValue): play.api.libs.json.JsObject = {
+    val paragraph = (input \ "paragraph").as[String]
+
+    val sentiment = mainSentiment(paragraph)
+    val sentimentString = sentiment match {
+      case Sentiment.NEGATIVE => "negative"
+      case Sentiment.POSITIVE => "positive"
+      case Sentiment.NEUTRAL => "neutral"
+    }
+
+    var timeDuration = extractTimeDuration(paragraph)
+
+    val gender = extractGender(paragraph)
+
+    Json.obj("sentiment" -> sentimentString, "timeDuration" -> timeDuration, "gender" -> gender)
+  }
 
   def mainSentiment(input: String): Sentiment = Option(input) match {
     case Some(text) if !text.isEmpty => extractSentiment(text)
@@ -39,6 +56,49 @@ object SentimentAnalyzer {
       .map(sentence => (sentence, sentence.get(classOf[SentimentCoreAnnotations.SentimentAnnotatedTree])))
       .map { case (sentence, tree) => (sentence.toString, Sentiment.toSentiment(RNNCoreAnnotations.getPredictedClass(tree))) }
       .toList
+  }
+
+  // this is very loose and will not account for both genders appearing in a paragraph
+  // it only deals with the first instance
+  def extractGender(text: String): String = {
+    var gender = "unknown"
+
+    // this is not DRY... would refactor later
+
+    val male_pattern = "\\bhe\\b".r
+    val male_result = male_pattern.findFirstIn(text) 
+    male_result match {
+      case Some(s) => gender = "male"
+      case None =>
+    }
+
+    val female_pattern = "\\bshe\\b".r
+    val female_result = female_pattern.findFirstIn(text) 
+    female_result match {
+      case Some(s) => gender = "female"
+      case None =>
+    }
+  
+    gender
+  }
+
+  def extractTimeDuration(text: String): String = {
+    var calculatedDuration : Long = -1
+
+    val pattern = "\\d{2}/\\d{2}/\\d{4}".r
+    val result = pattern.findAllIn(text) 
+    val dates = new ListBuffer[Long]()
+    val format = new java.text.SimpleDateFormat("MM/dd/yyyy")
+    result foreach { date =>
+        dates += format.parse(date).getTime()
+    }
+    if(dates.length>=2) {
+      val orderedDates = dates.sorted
+      val day_in_ms = 1000 * 60 * 60 * 24;
+      calculatedDuration = ((orderedDates.last-orderedDates.head)/day_in_ms) + 1
+    }
+
+    calculatedDuration.toString
   }
 
 }
--- src/test/scala/SentimentAnalyzerSpec.scala
+++ src/test/scala/SentimentAnalyzerSpec.scala
@@ -1,62 +1,43 @@
 package com.shekhargulati.sentiment_analyzer
 
 import org.scalatest.{FunSpec, Matchers}
-import play.api.libs.json._
 
 class SentimentAnalyzerSpec extends FunSpec with Matchers {
 
   describe("sentiment analyzer") {
 
     it("should return POSITIVE when input has positive emotion") {
-      val jsonString = """{ "paragraph": "Scala is a great general purpose language." }"""
-      val jsonObject = Json.parse(jsonString)
-
-      val result = SentimentAnalyzer.processJson(jsonObject)
-
-      val sentimentResult = (result \ "sentiment").as[String]
-      sentimentResult should be("positive")
+      val input = "Scala is a great general purpose language."
+      val sentiment = SentimentAnalyzer.mainSentiment(input)
+      sentiment should be(Sentiment.POSITIVE)
     }
 
     it("should return NEGATIVE when input has negative emotion") {
-      val jsonString = """{ "paragraph": "Dhoni laments bowling, fielding errors in series loss" }"""
-      val jsonObject = Json.parse(jsonString)
-
-      val result = SentimentAnalyzer.processJson(jsonObject)
-
-      val sentimentResult = (result \ "sentiment").as[String]
-      sentimentResult should be("negative")
+      val input = "Dhoni laments bowling, fielding errors in series loss"
+      val sentiment = SentimentAnalyzer.mainSentiment(input)
+      sentiment should be(Sentiment.NEGATIVE)
     }
 
     it("should return NEUTRAL when input has no emotion") {
-      val jsonString = """{ "paragraph": "I am reading a book" }"""
-      val jsonObject = Json.parse(jsonString)
-
-      val result = SentimentAnalyzer.processJson(jsonObject)
-
-      val sentimentResult = (result \ "sentiment").as[String]
-      sentimentResult should be("neutral")
+      val input = "I am reading a book"
+      val sentiment = SentimentAnalyzer.mainSentiment(input)
+      sentiment should be(Sentiment.NEUTRAL)
     }
 
     describe("COTA supplied texts") {
 
       it("should return NEUTRAL when input has mixed emotion") {
-        val jsonString = """{ "paragraph": "John downloaded the Pokemon Go app on 07/15/2016. By 07/22/2016, he was on level 24. Initially, he was very happy with the app. However, he soon became very disappointed with the app because it was crashing very often. As soon as he reached level 24, he uninstalled the app." }"""
-        val jsonObject = Json.parse(jsonString)
-
-        val result = SentimentAnalyzer.processJson(jsonObject)
-
-        val sentimentResult = (result \ "sentiment").as[String]
-        sentimentResult should be("neutral")
+        val input = "John downloaded the Pokemon Go app on 07/15/2016. By 07/22/2016, he was on level 24. Initially, he was very happy with the app. However, he soon became very disappointed with the app because it was crashing very often. As soon as he reached level 24, he uninstalled the app."
+        val sentiment = SentimentAnalyzer.mainSentiment(input)
+        sentiment should be(Sentiment.NEUTRAL)
       }
         
+
+/** this might be failing as it has multiple sentences and need to break it down into single sentences for sentiment */
       it("should return POSITIVE affect") {
-        val jsonString = """{ "paragraph": "Hua Min liked playing tennis. She first started playing on her 8th birthday - 07/07/1996. Playing tennis always made her happy. She won her first tournament on 08/12/2010. However, on 04/15/2015 when she was playing at the Flushing Meadows, she had a serious injury and had to retire from her tennis career." }"""
-        val jsonObject = Json.parse(jsonString)
-
-        val result = SentimentAnalyzer.processJson(jsonObject)
-
-        val sentimentResult = (result \ "sentiment").as[String]
-        sentimentResult should be("positive")
+        val input = "Hua Min liked playing tennis. She first started playing on her 8th birthday - 07/07/1996. Playing tennis always made her happy. She won her first tournament on 08/12/2010. However, on 04/15/2015 when she was playing at the Flushing Meadows, she had a serious injury and had to retire from her tennis career." 
+        val sentiment = SentimentAnalyzer.extractSentiments(input)
+        sentiment should be(Sentiment.POSITIVE)
       }
 
     }
